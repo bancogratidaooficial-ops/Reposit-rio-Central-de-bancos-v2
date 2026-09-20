f953422696f9a75808c48fc386560dd14272e9ab
bancogratidaooficial-ops <bancogratidao.oficial@gmail.com>
2026-09-20 01:42:36 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'v17-cofre-v2';
+const FILES = ['./','./index.html','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
+});
